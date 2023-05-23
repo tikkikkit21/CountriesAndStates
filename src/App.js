c8dfe2b743da840715639d254f5eafce62aee161
ttcui <ttcui@vt.edu>
2023-05-23 14:54:43 -0400

    Finished basic layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 function App() {
     return (
         <div className="app">
-            <h1>Hello, World!</h1>
-            <button className="country" onClick={handleClick}>Countries</button>
+            <h1>Countries and States</h1>
+            <button className="dropdown" onClick={handleClickCountry}>Countries</button>
+            <button className="dropdown" onClick={handleClickState}>States</button>
         </div>
     );
 }
 
 export default App;
 
-function handleClick() {
-    console.log("I have been clicked");
+function handleClickCountry() {
+    console.log("Country button clicked");
+}
+
+function handleClickState() {
+    console.log("State button clicked")
 }

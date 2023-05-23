2230e87abb3d18883aea017b9da692dd084137f5
ttcui <ttcui@vt.edu>
2023-05-23 14:38:43 -0400

    Added initial button for dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>Hello, World!</h1>
+            <button className="country" onClick={handleClick}>Countries</button>
+        </div>
+    );
 }
 
 export default App;
+
+function handleClick() {
+    console.log("I have been clicked");
+}

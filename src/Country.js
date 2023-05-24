7c8d3c4945d921e6f392689e288de896d9e9e6bf
ttcui <ttcui@vt.edu>
2023-05-24 09:19:56 -0400

    Finished fetching countries and mapping to components

--- src/Country.js
+++ src/Country.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import DropButton from './DropButton';
+import DropItem from './DropItem';
 
-function Country({name}) {
-    return <button className='country'>{name}</button>
+function Country() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://xc-countries-api.fly.dev/api/countries/")
+        .then(res => res.json())
+        .then(data => {
+            setData(data);
+        })
+        .catch(err => console.error(err));
+    })
+
+    function handleClick() {
+
+    }
+
+    return (
+        <div className="country">
+            <DropButton name="Country" onClick={handleClick}/>
+            {
+                data.map(d => <DropItem name={d.name}/>)
+            }
+        </div>
+    )
 }
 
 export default Country;

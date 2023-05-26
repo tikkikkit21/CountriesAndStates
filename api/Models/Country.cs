12181bf864fd4ca251f80422971a806f7ef282e8
ttcui <ttcui@vt.edu>
2023-05-26 10:51:46 -0400

    Added models
    Models are the blueprints for each type of item in a database collection

---
+++ api/Models/Country.cs
@@ -0,0 +1,8 @@
+namespace XC.Models;
+
+public class Country
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? code { get; set; }
+}
---
+++ api/Models/State.cs
@@ -0,0 +1,9 @@
+namespace XC.Models;
+
+public class State
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? code { get; set; }
+    public int countryId { get; set; }
+}

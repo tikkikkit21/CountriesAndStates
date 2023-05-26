2c1aa171f694e8a269b187c5e010dfb17f7fff7e
ttcui <ttcui@vt.edu>
2023-05-26 11:00:09 -0400

    Added base HTTP code via scaffolding
    Used the dotnet-aspnet-codegenerator scaffolding engine to generate
    basic HTTP request handlers

---
+++ api/Controllers/StateController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XC.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private readonly StateContext _context;
+
+        public StateController(StateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/State
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<State>>> GetStates()
+        {
+          if (_context.States == null)
+          {
+              return NotFound();
+          }
+            return await _context.States.ToListAsync();
+        }
+
+        // GET: api/State/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<State>> GetState(int id)
+        {
+          if (_context.States == null)
+          {
+              return NotFound();
+          }
+            var state = await _context.States.FindAsync(id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return state;
+        }
+
+        // PUT: api/State/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutState(int id, State state)
+        {
+            if (id != state.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(state).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/State
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<State>> PostState(State state)
+        {
+          if (_context.States == null)
+          {
+              return Problem("Entity set 'StateContext.States'  is null.");
+          }
+            _context.States.Add(state);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetState", new { id = state.id }, state);
+        }
+
+        // DELETE: api/State/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteState(int id)
+        {
+            if (_context.States == null)
+            {
+                return NotFound();
+            }
+            var state = await _context.States.FindAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            _context.States.Remove(state);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StateExists(int id)
+        {
+            return (_context.States?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}

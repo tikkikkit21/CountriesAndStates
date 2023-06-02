0ca20856b83a47a972c00c0cb67360dc2e99493c
ttcui <ttcui@vt.edu>
2023-06-02 11:03:01 -0400

    Fix navbar not being fixed during window resize

--- ui/src/Navbar.js
+++ ui/src/Navbar.js
@@ -10,16 +10,14 @@ function MyNavbar({ home, links }) {
     });
 
     return (
-        <Navbar bg="dark" variant="dark" expand="sm">
-            <Container>
-                <Navbar.Brand href="/">{home}</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {barLinks}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+        <Navbar className="p-2" bg="dark" variant="dark" expand="sm">
+            <Navbar.Brand href="/">{home}</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    {barLinks}
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }

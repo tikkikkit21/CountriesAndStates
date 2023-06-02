c9770b6f21a8e00d231610b49f6d105af4ae9f67
ttcui <ttcui@vt.edu>
2023-06-02 10:50:36 -0400

    Improve dropdown responsiveness and spacing

--- ui/src/pages/GetData.js
+++ ui/src/pages/GetData.js
@@ -2,26 +2,33 @@ import React from 'react';
 import Country from '../Country';
 import State from '../State';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function GetData({code, update, onChange}) {
     return (
-        <div className="container py-2 px-5">
-            <h1 className="text-center pb-3">Lookup Data</h1>
-            <div className="row d-flex justify-content-center ">
-                <Country
-                    id="countries"
-                    code={code}
-                    update={update}
-                    value="code"
-                    onChange={onChange}
-                />
-                <State id="states"
-                    code={code}
-                    update={update}
-                />
-            </div>
-        </div>
+        <Container>
+            <h1 className="text-center pt-2 pb-3">Lookup Data</h1>
+            <Row className="d-flex justify-content-center">
+                <Col className="p-2" xs={7} sm={5} md={4} lg={3}>
+                    <Country
+                        id="countries"
+                        code={code}
+                        update={update}
+                        value="code"
+                        onChange={onChange}
+                    />
+                </Col>
+                <Col className="p-2" xs={7} sm={5} md={4} lg={3}>
+                    <State id="states"
+                        code={code}
+                        update={update}
+                    />
+                </Col>
+            </Row>
+        </Container>
     );
-
 }
 
 export default GetData;

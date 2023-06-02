64ef30649c3e62d8b89c48275f6033e7e80a552d
ttcui <ttcui@vt.edu>
2023-06-02 10:57:07 -0400

    Fix vertical alignment for some col's

--- ui/src/pages/AddData.js
+++ ui/src/pages/AddData.js
@@ -12,28 +12,27 @@ function AddData({code, update, onClickCountry, onClickState}) {
         <Container>
             <h1 className="text-center pt-2 pb-3">Add Data</h1>
             <h2>Add a country</h2>
-            <Row className="pb-5">
-                <Col xs={7} sm={4} md={3} lg={2}>
+            <Row className="pb-5 d-flex align-items-center">
+                <Col className="p-2" xs={7} sm={4} md={3} lg={2}>
                     <Input name="code" id="code-country"/>
                 </Col>
-                <Col xs={7} sm={6} md={5} lg={4}>
+                <Col className="p-2" xs={7} sm={6} md={5} lg={4}>
                     <Input name="name" id="name-country"/>
                 </Col>
-                <Col xs={7} sm={2} lg={1}>
+                <Col className="p-2" xs={7} sm={2} lg={1}>
                     <Button variant="primary" as="input" type="submit" onClick={onClickCountry}/>
                 </Col>
             </Row>
 
             <h2>Add a state</h2>
-            <Row>
-                <Col xs={7} sm={4} md={3} lg={2}>
+            <Row className="pb-5 d-flex align-items-center">
+                <Col className="p-2" xs={7} sm={4} md={3} lg={2}>
                     <Input name="code" id="code-state" />
                 </Col>
-                <Col xs={7} sm={6} md={5} lg={4}>
+                <Col className="p-2" xs={7} sm={6} md={5} lg={4}>
                     <Input name="name" id="name-state" />
                 </Col>
-                
-                <Col xs={7} sm={10} md={4} lg={4}>
+                <Col className="p-2" xs={7} sm={10} md={4} lg={4}>
                     <Country
                         id="countries-state"
                         code={code}
@@ -42,7 +41,7 @@ function AddData({code, update, onClickCountry, onClickState}) {
                     />
                 </Col>
 
-                <Col xs={7} sm={2} md={1}>
+                <Col className="p-2" xs={7} sm={2} md={1}>
                     <Button variant="primary" as="input" type="submit" onClick={onClickState}/>
                 </Col>
             </Row>

450a73e4d3181724703ae2691f3c3c98a25ceada
ttcui <ttcui@vt.edu>
2023-06-05 12:47:24 -0400

    Setup files for unit testing

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,9 +5,14 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  server: {
-    port: 3000,
-    host: true
-  }
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    server: {
+        port: 3000,
+        host: true
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/tests/setup.js',
+    },
 });
